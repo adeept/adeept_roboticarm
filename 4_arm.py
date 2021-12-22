612703001c04ece814b64b32b57631d09ab3c17a
Devin <support@adeept.com>
2021-12-22 17:48:25 +0800

    update

--- 4_arm.py
+++ 4_arm.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python3
-# File name   : servo.py
-# Description : Control Servos
-# Author      : William
-# Date        : 2019/02/23
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import PCF8591 as ADC
 import Adafruit_PCA9685
 import time
 
-
-# pwm0_direction = 1
-# pwm1_direction = 0
-# pwm2_direction = 1
-# pwm3_direction = 1
-# pwm4_direction = 0
+L_btn = 11
+R_btn = 12
 
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
-
-
-# pwm0_init = 300
-# pwm0_max  = 450
-# pwm0_min  = 150
-# pwm0_pos  = pwm0_init
-
-angle = [300, 300, 300, 300]
+# pwm_init = 300        90°
+# pwm_max  = 500        180° 
+# pwm_min  = 100        0°
+ 
+angle = [300, 300, 300, 300]    # The angle of all servos is 90°.
 speed = 7 # servo rotation speed.
 forward = 1
 reverse = -1
 
 
+
+mark = None
+state_num = None
+state_mark = None
+
 def setup():
     ADC.setup(0X48)
-    # global state
+    GPIO.setmode(GPIO.BOARD)	# Numbers GPIOs by physical location
+    GPIO.setup(L_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# Setup button pin as input an pull it up
+    GPIO.setup(R_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# Setup button pin as input an pull it up
+    
+    pwm.set_pwm(0, 0, 300)
+    pwm.set_pwm(1, 0, 300)
+    pwm.set_pwm(2, 0, 300)
+    pwm.set_pwm(3, 0, 300)
+
 
 
 def ctrl_range(raw, max_genout, min_genout):
@@ -47,16 +49,19 @@ def ctrl_range(raw, max_genout, min_genout):
 
 def rotation(ID, direction, speed):
     global angle
-    if direction == 1:
-        angle[ID] += speed
+    if ID == None:
+        pass
     else:
-        angle[ID] -= speed
-    if angle[ID] >= 500:
-        angle[ID] = 500
-    if angle[ID] <=100:
-        angle[ID] = 100
-    print(angle[ID])
-    pwm.set_pwm(ID, 0, angle[ID])
+        if direction == 1:
+            angle[ID] += speed
+        else:
+            angle[ID] -= speed
+        if angle[ID] >= 500:
+            angle[ID] = 500
+        if angle[ID] <=100:
+            angle[ID] = 100
+
+        pwm.set_pwm(ID, 0, angle[ID])
 
 def move_servo(value):
     if value == 1:          # servo 1
@@ -76,53 +81,74 @@ def move_servo(value):
     elif value == -4:
         rotation(3, reverse, speed)
     else:
-        pass
+        rotation(None, reverse, speed)
     
-def joystick(): #get joystick result
-    # state = ['home', 'up', 'down', 'left', 'right']
-    value = 0   
-    if ADC.read(0) <= 30:   # servo 1
+def joystick(): #get joystick result.
+    global state_num, state_mark
+    state = ['home','L-pressed', 'L-up', 'L-down', 'L-left', 'L-right',\
+             'R-home','R-pressed', 'R-up', 'R-down', 'R-left', 'R-right']
+    value = None
+    if GPIO.input(L_btn) == 0:
+        value = 5
+        state_num = 1
+    elif GPIO.input(R_btn) == 0:
+        value = 6
+        state_num = 7
+    else:
+        value = 0
+        state_num = 0
+
+    if ADC.read(1) <= 30:  # servo 1
         value = 1 
-    elif ADC.read(0) >= 210: 
+        state_num = 4
+    elif ADC.read(1)>= 210 :   # servo 1
         value = -1
+        state_num = 5
 
-    if ADC.read(1) <= 30:   # servo 2
+    if ADC.read(0) >= 210:   # servo 2
         value = 2
-    elif ADC.read(1) >= 210: 
+        state_num = 2
+    elif ADC.read(0) <= 30: 
         value = -2
+        state_num = 3
 
-    if ADC.read(2) <= 30:   # servo 3
+    if ADC.read(2) <= 30: # servo 3
         value = 3
-    elif ADC.read(2) >= 210: 
+        state_num = 9
+    elif ADC.read(2)>= 210 :   # servo 3
         value = -3 
+        state_num = 8
 
     if ADC.read(3) <= 30:   # servo 4
         value = 4
+        state_num = 10
     elif ADC.read(3) >= 210: 
         value = -4
+        state_num = 11
     
+    if state_mark != state_num: # print state.
+        print(state[state_num])
+        state_mark = state_num
     return value
 
 def loop():
-    mark = 10
+    global mark
     value = joystick()
     move_servo(value)
     if mark != value:
-        print(value)
+        # print(value)
         mark = value
     time.sleep(0.05)
 
 def destroy():
-    pass    
+    GPIO.cleanup()   
 
 
 if __name__ == '__main__':
-    # try:
     setup()
-    while True:
-        loop()
-        # time.sleep(1)
-    # except:
-    #     destroy()
-    # print("quit")
+    try:
+        while True:
+            loop()
+    except:
+        destroy()
 
